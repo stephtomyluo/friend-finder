4e5fc7d286ade1fe50df17bcf4a362b4a9f0ecea
4stru <4.struong@kscholars.org>
2019-12-08 22:02:47 -0600

    friends

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+let friends = [
+    {
+        name: "Leslie",
+        photo: "https://www.washingtonpost.com/resizer/zoNwBbtUN1IIctm40r8XOFSsQSg=/767x0/smart/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/JBAXK7JIHA63PMOM2VG42BJF5I.jpg",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Ron",
+        photo: "https://media0ch-a.akamaihd.net/73/51/6be6abbc93161c83db99024f6359d758-why-ron-swanson-was-the-worst-character-on-parks-rec.jpg",
+        scores: [
+
+        ]
+    },
+    {
+        name: "April",
+        photo: "https://i.pinimg.com/originals/ee/01/99/ee0199bad119fecaa006c40ea12b2556.jpg",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Andy",
+        photo: "https://thoughtcatalog.files.wordpress.com/2015/06/tumblr_ne4xh0lhce1ttqoz7o7_1280.jpg?w=786&h=524",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Ben",
+        photo: "https://i.redd.it/p6v5qvmunom21.jpg",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Tom",
+        photo: "https://www.incimages.com/uploaded_files/image/970x450/getty_463381742_2000133020009280116_75578.jpg",
+        scores: [
+
+        ]
+    },
+
+
+]
