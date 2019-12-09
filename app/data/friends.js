20edfbbf1cfbfc2fca0377b14dd403527cc98483
4stru <4.struong@kscholars.org>
2019-12-08 22:30:46 -0600

    scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,42 +3,96 @@ let friends = [
         name: "Leslie",
         photo: "https://www.washingtonpost.com/resizer/zoNwBbtUN1IIctm40r8XOFSsQSg=/767x0/smart/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/JBAXK7JIHA63PMOM2VG42BJF5I.jpg",
         scores: [
-
+            '5',
+            '3',
+            '3',
+            '5',
+            '1',
+            '1',
+            '3',
+            '1',
+            '5',
+            '1'
         ]
     },
     {
         name: "Ron",
         photo: "https://media0ch-a.akamaihd.net/73/51/6be6abbc93161c83db99024f6359d758-why-ron-swanson-was-the-worst-character-on-parks-rec.jpg",
         scores: [
-
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3'
         ]
     },
     {
         name: "April",
         photo: "https://i.pinimg.com/originals/ee/01/99/ee0199bad119fecaa006c40ea12b2556.jpg",
         scores: [
-
+            '5',
+            '4',
+            '3',
+            '4',
+            '5',
+            '5',
+            '1',
+            '5',
+            '1',
+            '5'
         ]
     },
     {
         name: "Andy",
         photo: "https://thoughtcatalog.files.wordpress.com/2015/06/tumblr_ne4xh0lhce1ttqoz7o7_1280.jpg?w=786&h=524",
         scores: [
-
+            '4',
+            '4',
+            '2',
+            '3',
+            '5',
+            '3',
+            '3',
+            '4',
+            '5',
+            '2'
         ]
     },
     {
         name: "Ben",
         photo: "https://i.redd.it/p6v5qvmunom21.jpg",
         scores: [
-
+            '2',
+            '5',
+            '1',
+            '3',
+            '4',
+            '3',
+            '3',
+            '2',
+            '4',
+            '1'
         ]
     },
     {
         name: "Tom",
         photo: "https://www.incimages.com/uploaded_files/image/970x450/getty_463381742_2000133020009280116_75578.jpg",
         scores: [
-
+            '5',
+            '1',
+            '5',
+            '1',
+            '1',
+            '4',
+            '5',
+            '2',
+            '4',
+            '4'
         ]
     },
 
